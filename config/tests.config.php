bc70add3dbee3562d9773bda7d22783afcf2324d
Juha Suni <juha.suni@mediacabinet.fi>
2013-04-29 12:59:09 +0300

    Adds config overrides for tests

---
+++ config/tests.config.php
@@ -0,0 +1,29 @@
+<?php
+return [
+    'doctrine' => [
+        'configuration' => [
+            'odm_default' => [
+                'default_db' => 'valu_file_system_test'
+            ]
+        ]
+    ],
+    'valu_so' => [
+        // Ensure that proxy classes are re-generated when they change
+        'proxy_auto_create_strategy' => 'mtime',
+        'services' => [
+            'ValuFileStorageLocalFile' => [
+                'options' => [
+                    'paths' => [
+                        'tmp' => realpath(__DIR__ .'/../tests/tmp'),
+                        'files' => realpath(__DIR__ .'/../tests/tmp'),
+                    ]
+                ],
+            ],
+        ],
+    ],
+    'file_storage' => [
+        'whitelist' => [
+            'tests' => 'file://.*/vendor/valu/valufilestorage/tests/resources/.*',
+        ]
+    ],
+];
